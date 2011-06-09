fe20b49a689344f2eb3a044d8a013e2d94c4fab9
Raúl Jiménez Ortega <hhkaos@hhkaos-desktop.(none)>
2011-06-10 01:23:34 +0200

    New version working

---
+++ trunk/src/community/templates/menu_sections.php
@@ -0,0 +1,20 @@
+<?php
+	$menu_sections=array(
+		array(
+			'name'=>'Home',
+			'url'=>'http://www.georemindme.com',
+		),
+		array(
+			'name'=>'APIs',
+			'url'=>'http://api.georemindme.com',
+		),
+		array(
+			'name'=>'Community',
+			'url'=>'http://team.georemindme.com',
+		),
+		array(
+			'name'=>'Jobs',
+			'url'=>'http://jobs.georemindme.com',
+		),
+	)
+?>
