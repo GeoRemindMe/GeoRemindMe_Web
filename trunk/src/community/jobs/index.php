ff004fccb6f1cc2ceb49002612498169bf515da6
Raúl Jiménez Ortega <hhkaos@hhkaos-desktop.(none)>
2011-06-10 00:40:41 +0200

    Adding new sections: jobs, teams, etc.

---
+++ trunk/src/community/jobs/index.php
@@ -0,0 +1,26 @@
+<?php
+require '../h2o/h2o.php';
+require '../templates/menu_sections.php';
+h2o::load('i18n');
+
+$var_template='jobs.html';
+$var_title='Empleo';
+$active_section= 'Jobs'; //Values= Names on ../templates/menu_sections.php
+
+$template = new H2o('../templates/'.$var_template, array(
+    'cache_dir' => dirname(__FILE__),
+    'searchpath' => 'h2o',
+    
+    /*'i18n' => array(
+		'locale' => 'fr',
+	)*/
+));
+
+echo $template->render(array(
+	'title'=> $var_title,
+	'menu_items'=>$menu_sections,
+	'active_section'=>$active_section,
+	)
+);
+
+
