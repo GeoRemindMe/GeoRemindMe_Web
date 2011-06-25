a272b0cd24baf5128652d25c230c7be99243b5fb
Raúl Jiménez Ortega <raul@georemindme.com>
2011-06-25 12:55:23 +0200

    Changed GPL to AGPL

--- src/webapp/geouser/properties.py
+++ src/webapp/geouser/properties.py
@@ -3,19 +3,19 @@
 This file is part of GeoRemindMe.
 
 GeoRemindMe is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+it under the terms of the Affero General Public License (AGPL) as published 
+by Affero, as published by the Free Software Foundation, either version 3 
+of the License, or (at your option) any later version.
 
 GeoRemindMe is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
 
-You should have received a copy of the GNU General Public License
+You should have received a copy of the GNU Affero General Public License
 along with GeoRemindMe.  If not, see <http://www.gnu.org/licenses/>.
 
 """
+
 import re
 from google.appengine.ext import db
 
