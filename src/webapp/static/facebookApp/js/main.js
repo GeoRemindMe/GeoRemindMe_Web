a875cd0c837d4d9504c773a07ed79d90b07422b6
Raul Jimenez Ortega <raul@georemindme.com>
2011-06-29 19:50:25 +0200

    Added follow/unfollow with AJAX and JQuery templates

--- src/webapp/static/facebookApp/js/main.js
+++ src/webapp/static/facebookApp/js/main.js
@@ -7,22 +7,15 @@ function facebookInit(config) {
     appId: Config.appId,
     xfbml: true,
     cookie : true, // enable cookies to allow the server to access the session
-    //~ channelUrl:
-      //~ window.location.protocol + '//' + window.location.host + '/js/channel.html'
   });
   FB.Event.subscribe('auth.sessionChange', handleSessionChange);
-  //~ FB.Event.subscribe('auth.logout', function() {
-      //~ alert("yepah2");
-      //~ FB.logout(function(response) {
-          //~ alert("yepah");
-          //~ });
-    //~ });
+
   FB.Canvas.setAutoResize();
 
   // ensure we're always running on apps.facebook.com
   // if we are opening http://localhost:8080/fb/ or
   // georemindme.appspot.com/fb we will be redirected
-  if (window == top) { goHome(); }
+  //~ if (window == top) { goHome(); }
 }
 
 function handleSessionChange(response) {
