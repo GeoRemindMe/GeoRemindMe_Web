66c686b35f4164cc7dbc9b32128def4b28481392
Javier Cordero <javier@georemindme.com>
2011-07-22 13:28:16 +0200

    add_suggestion_follower

--- src/webapp/facebookApp/views.py
+++ src/webapp/facebookApp/views.py
@@ -193,7 +193,6 @@ def edit_suggestion(request,suggestion_id):
     from geoalert.forms import SuggestionForm
     s = Suggestion.objects.get_by_id(suggestion_id)
     return  render_to_response('add_suggestion.html', {
-                                                        
                                                         'eventid':suggestion_id,
                                                         'name': s.name,
                                                         'poi_id': s.poi.id,
