3cb1deb57a373471e6aab599b82c63331e3ad353
Javier Cordero <jneight@gmail.com>
2011-07-25 11:13:10 +0200

    profile timeline y activity timeline

--- src/webapp/geouser/models.py
+++ src/webapp/geouser/models.py
@@ -206,13 +206,9 @@ class User(polymodel.PolyModel, HookedModel):
         
     def get_activity_timeline(self, page=1, query_id=None):
         if query_id is not None:
-            dict_id = memcache.get(memcache.get('%squery_%s' % (memcache.version, query_id)))
-            if dict_id is not None:
-                chrono_id = dict_id['chronology']
-                time_id = dict_id['timeline']
-            else:
-                chrono_id = None
-                time_id = None
+            query_id = query_id.split('_')
+            chrono_id = query_id[0]
+            time_id = query_id[1]
         else:
             chrono_id = None
             time_id = None
@@ -220,11 +216,6 @@ class User(polymodel.PolyModel, HookedModel):
         timeline = self.get_timelinesystem(page=page, query_id=time_id)
         chronology[1].extend(timeline[1])
         chronology[1].sort(key=lambda x: x['modified'], reverse=True)
-        dict_id = {
-                   'chronology': chronology[0],
-                   'timeline': timeline[0]
-                   }
-        memcache.set('%squery_%s_%s' % (memcache.version, chronology[0], timeline[0]), dict_id)
         chronology[0] = '%s_%s' % (chronology[0], timeline[0])
         return chronology
         
