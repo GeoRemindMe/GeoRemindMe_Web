8b2370ada697678af43e64a254d76fce68ba45dd
Javier Cordero <javier@georemindme.com>
2011-07-26 08:34:43 +0200

    func. timelines por ajax

--- src/webapp/geouser/helpers_acc.py
+++ src/webapp/geouser/helpers_acc.py
@@ -86,17 +86,17 @@ class UserTimelineHelper(object):
                         'vote_counter': Vote.objects.get_vote_counter(timeline.instance.key()) if timeline.instance is not None else None
                             } 
                            for timeline in timelines]]
-            
-        elif not querier.are_friends(db.Key.from_path(User.kind(), userid)):
-            return [p.id, [{'id': timeline.id, 'created': timeline.created, 
-                        'modified': timeline.modified,
-                        'msg': timeline.msg, 'username':timeline.user.username, 
-                        'msg_id': timeline.msg_id,
-                        'instance': timeline.instance if timeline.instance is not None else None,
-                        'has_voted':  Vote.objects.user_has_voted(querier, timeline.instance.key()) if timeline.instance is not None else None,
-                        'vote_counter': Vote.objects.get_vote_counter(timeline.instance.key()) if timeline.instance is not None else None
-                            }
-                           for timeline in timelines if timeline._is_public()]]
+        elif querier.is_authenticated():
+            if not querier.are_friends(db.Key.from_path(User.kind(), userid)):
+                return [p.id, [{'id': timeline.id, 'created': timeline.created, 
+                            'modified': timeline.modified,
+                            'msg': timeline.msg, 'username':timeline.user.username, 
+                            'msg_id': timeline.msg_id,
+                            'instance': timeline.instance if timeline.instance is not None else None,
+                            'has_voted':  Vote.objects.user_has_voted(querier, timeline.instance.key()) if timeline.instance is not None else None,
+                            'vote_counter': Vote.objects.get_vote_counter(timeline.instance.key()) if timeline.instance is not None else None
+                                }
+                               for timeline in timelines if timeline._is_public()]]
         
         return [p.id, [{'id': timeline.id, 'created': timeline.created, 
                         'modified': timeline.modified,
