30dad3d5d9412710d2038d71ee43770473cfdb15
hhkaos <hhkaos@server314.(none)>
2011-08-17 23:50:47 +0200

    Adding a bash script to run the local server

---
+++ src/webapp/run_server.py
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Path to google app engine server
+SERV=/home/hhkaos/Apps/google_appengine/dev_appserver.py
+#Path to git repository
+REPO=/home/hhkaos/workspace/GeoRemindMe_Web
+
+$SERV \
+	--datastore_path=$REPO/src/webapp/fixtures/dev_appserver.datastore \
+	$REPO/src/webapp/
