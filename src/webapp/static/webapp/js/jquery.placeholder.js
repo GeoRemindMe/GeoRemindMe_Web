5c455c8beb467c26f37124f0ebe3cead70cda367
Rubén Dugo <rdugomartin@gmail.com>
2011-08-29 10:25:05 +0200

    arreglado plugin

--- src/webapp/static/webapp/js/jquery.placeholder.js
+++ src/webapp/static/webapp/js/jquery.placeholder.js
@@ -1,83 +1,18 @@
 jQuery.fn.placeholder = function() 
 {
-	$('textarea').each(function()
-	{
-		var placeholder = $(this).attr("placeholder");
-		$(this).html(placeholder);
-		
-		$(this).css({color:'#999999',"font-style":"italic"});
-		
-		$(this).bind('focus', function()
+	return this.each(function(){
+		if ($(this).is("textarea"))
 		{
-			if($(this).html()==placeholder)
-			{
-				$(this).html("");
-				$(this).css({color:'#000'});
-			}
-		}
-		)
-		
-		$(this).bind('blur', function()
-		{
-			if($(this).html()=="")
-			{
-				$(this).html(placeholder);
-				$(this).css({color:'#999999'});
-			}
-		}
-		)
-	}
-	);
-	
-	var counter=0;
-	
-	$('input').each(function()
-	{
-		var id = $(this).attr("id");
-		var tabindex = $(this).attr("tabindex");
-		var placeholder = $(this).attr("placeholder");
-		
-		if($(this).attr("type")=="password")
-		{
-			var thisdummy = 'dummy' + counter;
+			var placeholder = $(this).attr("placeholder");
+			$(this).html(placeholder);
 			
-			$(this).hide();
-			$(this).after('<input type="text" id="' + thisdummy + '"/>');
-			$('#' + thisdummy).val(placeholder);
-			if (tabindex) $('#' + thisdummy).attr("tabIndex",tabindex);
-			$('#' + thisdummy).css({color:'#999999',"font-style":"italic"});
-			
-			counter++;
-			
-			$('#' + thisdummy).bind('focus', function()
-			{
-				$("#" + id).show().val("");
-				$("#" + id).show().focus();
-				$(this).hide();
-			}
-			)
-			
-			$(this).bind('blur', function()
-			{
-				if($(this).val()=="")
-				{
-					$(this).hide();
-					$("#" + thisdummy).show();
-				};
-			}
-			)
-		}
-		
-		if($(this).attr("type")=="text")
-		{
-			$(this).val(placeholder);
 			$(this).css({color:'#999999',"font-style":"italic"});
 			
 			$(this).bind('focus', function()
 			{
-				if($(this).val()==placeholder)
+				if($(this).html()==placeholder)
 				{
-					$(this).val("");
+					$(this).html("");
 					$(this).css({color:'#000'});
 				}
 			}
@@ -85,15 +20,78 @@ jQuery.fn.placeholder = function()
 			
 			$(this).bind('blur', function()
 			{
-				if($(this).val()=="")
+				if($(this).html()=="")
 				{
-					$(this).val(placeholder);
+					$(this).html(placeholder);
 					$(this).css({color:'#999999'});
 				}
 			}
 			)
 		}
-	}
-	);
-};
+		else if ($(this).is("input")) {
+		
+		var counter=0;
+			var id = $(this).attr("id");
+			var tabindex = $(this).attr("tabindex");
+			var placeholder = $(this).attr("placeholder");
+			
+			if($(this).attr("type")=="password")
+			{
+				var thisdummy = 'dummy' + counter;
+				
+				$(this).hide();
+				$(this).after('<input type="text" id="' + thisdummy + '"/>');
+				$('#' + thisdummy).val(placeholder);
+				if (tabindex) $('#' + thisdummy).attr("tabIndex",tabindex);
+				$('#' + thisdummy).css({color:'#999999',"font-style":"italic"});
+				
+				counter++;
+				
+				$('#' + thisdummy).bind('focus', function()
+				{
+					$("#" + id).show().val("");
+					$("#" + id).show().focus();
+					$(this).hide();
+				}
+				)
+				
+				$(this).bind('blur', function()
+				{
+					if($(this).val()=="")
+					{
+						$(this).hide();
+						$("#" + thisdummy).show();
+					};
+				}
+				)
+			}
+			
+			if($(this).attr("type")=="text")
+			{
+				$(this).val(placeholder);
+				$(this).css({color:'#999999',"font-style":"italic"});
+				
+				$(this).bind('focus', function()
+				{
+					if($(this).val()==placeholder)
+					{
+						$(this).val("");
+						$(this).css({color:'#000'});
+					}
+				}
+				)
+				
+				$(this).bind('blur', function()
+				{
+					if($(this).val()=="")
+					{
+						$(this).val(placeholder);
+						$(this).css({color:'#999999'});
+					}
+				}
+				)
+			}
+		}
+	});
+}
 
