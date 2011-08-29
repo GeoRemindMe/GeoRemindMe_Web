04531dbf964265ca02b9ceef3a506ff33429e94f
Javier Cordero <jneight@gmail.com>
2011-08-29 12:33:09 +0200

    MapService.get

--- src/webapp/service/api/main.py
+++ src/webapp/service/api/main.py
@@ -3,17 +3,18 @@ from google.appengine.ext.webapp import util
 
 from protorpc.webapp import service_handlers
 
-from service.api.one import timelineservice, suggestionservice
+from service.api.one import timelineservice, suggestionservice, mapservice
 from service.api import middleware
 
 # Register mapping with application.
 application = webapp.WSGIApplication(
                                      service_handlers.service_mapping(
                                           [('/api/1/TimelineService', timelineservice.TimelineService),
-                                           ('/api/1/SuggestionService', suggestionservice.SuggestionService)
+                                           ('/api/1/SuggestionService', suggestionservice.SuggestionService),
+                                           ('/api/1/MapService', mapservice.MapService,)
                                            ]),
                                           debug=True)
-#application = middleware.OAuthware(application)
+application = middleware.OAuthware(application)
 
 def main():
     util.run_wsgi_app(application)
