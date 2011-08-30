831441a8599b0881b92684c6f05c676d5717cbf7
Jose Luis Lopez Pino <jllopezpino@gmail.com>
2011-08-30 12:11:04 +0200

    small change in run_server.py

--- src/webapp/run_server.py
+++ src/webapp/run_server.py
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #Path to google app engine server
-SERV=$/Apps/google_appengine/dev_appserver.py
+SERV=~/Apps/google_appengine/dev_appserver.py
 #Path to git repository
-REPO=$/workspace/GeoRemindMe_Web
+REPO=~/workspace/GeoRemindMe_Web
 
 $SERV \
 	--datastore_path=$REPO/src/fixtures/dev_appserver.datastore \
