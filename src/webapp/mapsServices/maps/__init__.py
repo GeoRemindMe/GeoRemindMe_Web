6198e16f95b5dbace6a16c1c70dcf778268dec82
Javier Cordero <jneight@gmail.com>
2011-09-27 13:48:16 +0200

    sugerencias cercanas

---
+++ src/webapp/mapsServices/maps/__init__.py
@@ -0,0 +1,47 @@
+#coding=utf-8
+
+
+from libs.httplib2 import Http
+from google.appengine.ext import db
+import memcache
+from django.utils import simplejson
+
+
+class MapsAPIError(Exception):
+    def __init__(self, type, message=''):
+        Exception.__init__(self, message)
+        self.type = type
+
+
+class MapsRequest(Http):
+    _reverse_url = 'http://maps.googleapis.com/maps/api/geocode/json?'
+    
+    def __init__(self, *args, **kwargs):
+        mem = memcache.mem.Client()
+        super(self.__class__, self).__init__(cache=mem, *args, **kwargs)
+        
+        
+    def get_address(self, location, sensor=False):
+        if not isinstance(location, db.GeoPt):
+            return MapsAPIError('invalid location')
+        url = self._reverse_url + 'latlng=%s,%s&sensor=%s' % (location.lat, 
+                                                    location.lon, 
+                                                    ('true' if sensor else 'false', self.key)
+                                                    )
+        return self._do_request(url)
+        
+    def _do_request(self, url, method='GET', body=None):
+        """
+            Realiza una peticion por GET a la direccion recibida
+            
+                :param url: direccion url a donde hacer la peticion
+                :type url: string
+                
+                :returns: diccionario con el resultado
+                :raises: :class:`GPAPIError`
+        """
+        response, content = self.request(url, method=method, body=body, headers=self.headers)
+        if response['status'] != 200:
+            raise MapsAPIError(response['status'], 'ERROR IN REQUEST')
+        json = simplejson.loads(content)
+        return json 
