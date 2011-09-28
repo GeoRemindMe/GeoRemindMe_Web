bfa47cc2c4fc7d72abf2f4f2660279312d20bbf3
Raúl Jiménez Ortega <raul@georemindme.com>
2011-09-28 15:20:38 +0200

    Working on layout

--- src/webapp/static/common/js/login.js
+++ src/webapp/static/common/js/login.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
                     GRM.nowait();
                 }
                 else {
-                    setTimeout(function(){window.location=msg._redirect;},3000);
+                    function(){window.location=msg._redirect;}
                 }
             }
         });
@@ -140,7 +140,7 @@ $(document).ready(function() {
                 if(typeof(data._redirect)!="undefined")
                 {
                    REDIRECT = data._redirect;
-                   setTimeout(function(){window.location = REDIRECT}, 3000);
+                   function(){window.location = REDIRECT}
                 }
              }
              
