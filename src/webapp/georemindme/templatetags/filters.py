faa3a8f0e13220b4487905da06f6163ad0ee1bdb
Ruben Dugo <rdugomartin@gmail.com>
2011-10-06 10:11:29 +0200

    fixed bug 'Fallo en descripcion de sugerencias con varias lineas'. added twitterize filter

--- src/webapp/georemindme/templatetags/filters.py
+++ src/webapp/georemindme/templatetags/filters.py
@@ -11,3 +11,14 @@ def private(value, arg):
     if callable(private):
         return private()
     return private
+
+
+@register.filter(name='twitterize')
+def twitterize(token):
+    import re
+    token =  re.sub(r'(@(\w+))', r'<a href="https://twitter.com/\2">\1</a>', token)
+    token =  re.sub(r'(#(\w+))', r'<a href="http://twitter.com/#!/search/\2">\1</a>', token)
+    
+    return token
+twitterize.is_safe = True
+
