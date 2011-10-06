626ad88e59529f7b44aeff86e375790f2aca9481
Javier Cordero <jneight@gmail.com>
2011-10-06 10:04:45 +0200

    generacion del sitemap

--- src/webapp/service/api/one/timelineservice.py
+++ src/webapp/service/api/one/timelineservice.py
@@ -52,5 +52,33 @@ class TimelineService(remote.Service):
                     t.url = a['instance'].get_absolute_url().encode("utf8")
             timelines.append(t)
         return Timelines(timelines=timelines, query_id = activity[0])
+    
+    @remote.method(GetActivityRequest, Timelines)
+    def get_jneight(self, request):
+        from os import environ
+        from geouser.models import User
+        user = User.objects.get_by_username('jneight')
+        activity = user.get_activity_timeline(request.query_id)
+        timelines = []
+        from time import mktime
+        from geovote.models import Comment, Vote
+        for a in activity[1]:
+            t = Timeline(msg=a['msg'], 
+                         msg_id = a['msg_id'],
+                         user=a['username'],
+                         created=int(mktime(a['modified'].utctimetuple()))
+                         )
+            if a['instance'] is not None:
+                if isinstance(a['instance'], Comment) or isinstance(a['instance'], Vote): 
+                    t.instance_id=a['instance'].instance.id
+                    t.instance_name=unicode(a['instance'].instance)
+                    if hasattr(a['instance'].instance,"get_absolute_url"):
+                        t.url = a['instance'].instance.get_absolute_url().encode("utf8")
+                else:
+                    t.instance_id=a['instance'].id
+                    t.instance_name=unicode(a['instance'])
+                    t.url = a['instance'].get_absolute_url().encode("utf8")
+            timelines.append(t)
+        return Timelines(timelines=timelines, query_id = '-'.join(activity[0]))
 
     
