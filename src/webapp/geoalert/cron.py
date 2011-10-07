9bb814cb8e8d0f11e4dcbef55a442a49127fd19b
Javier Cordero <jneight@gmail.com>
2011-10-07 09:38:30 +0200

    missed cron

--- src/webapp/geoalert/cron.py
+++ src/webapp/geoalert/cron.py
@@ -8,24 +8,4 @@ from models import Suggestion
 
 @cron_required
 def cron_suggestions(request, cursor):
-    keys = []
-    if cursor is None:
-        suggs = Suggestion.all().filter('_vis =', 'public').fetch(500)
-    else:
-        suggs = Suggestion.all().filter('_vis =', 'public').with_cursor(cursor).fetch(500)
-    keys.extend(suggs)
-    while len(suggs) == 500:
-        keys.extend(suggs)
-        q = Suggestion.all().filter('_vis =', 'public').filter('__key__ >', suggs[-1].key())
-        suggs = q.fetch(500)
-        keys.extend(suggs)
-        
-
-    return [x.get_absolute_url() for x in keys]
-    from google.appengine.ext.deferred import defer, PermanentTaskFailure 
-    try:
-        defer(sender.insert_ft, _queue="fusiontables")
-    except PermanentTaskFailure, e:
-        from georemindme.models_utils import _Do_later_ft
-        later = _Do_later_ft(instance_key=sender.key())
-        later.put()
+   pass
