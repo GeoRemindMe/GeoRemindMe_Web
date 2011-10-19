54ad76524754aa2d3bd9d7b8b55def54ff4fb6f6
Javier Cordero <jneight@gmail.com>
2011-10-19 09:41:19 +0200

    mas

--- src/webapp/appengine_config.py
+++
@@ -1,9 +0,0 @@
-webapp_django_version = '1.2'
-#import os, sys
-#from google.appengine.dist import use_library
-#
-#for k in [k for k in sys.modules if k.startswith('django')]: 
-#    del sys.modules[k] 
-#    
-#os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
-#use_library('django', '1.2')
